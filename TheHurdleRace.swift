8003b67c55664bf32ed4b0375a421f465556d3ea
iCodeAppsNithya <155045110+iCodeAppsNithya@users.noreply.github.com>
2023-12-28 14:38:27 +0530

    The Hurdle Race
    https://www.hackerrank.com/challenges/the-hurdle-race

---
+++ TheHurdleRace.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+func hurdleRace(k: Int, height: [Int]) -> Int {
+    if let largestNo = height.max(), k < largestNo {
+        return largestNo - k
+    } else {
+        return 0
+    }
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let firstMultipleInputTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+let firstMultipleInput = firstMultipleInputTemp.split(separator: " ").map{ String($0) }
+
+guard let n = Int(firstMultipleInput[0])
+else { fatalError("Bad input") }
+
+guard let k = Int(firstMultipleInput[1])
+else { fatalError("Bad input") }
+
+guard let heightTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let height: [Int] = heightTemp.split(separator: " ").map {
+    if let heightItem = Int($0) {
+        return heightItem
+    } else { fatalError("Bad input") }
+}
+
+guard height.count == n else { fatalError("Bad input") }
+
+let result = hurdleRace(k: k, height: height)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
