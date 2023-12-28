a319f6d2a9a097eb19b8574967280259a898413c
iCodeAppsNithya <155045110+iCodeAppsNithya@users.noreply.github.com>
2023-12-28 12:17:54 +0530

    SubArray Division Challenge
    https://www.hackerrank.com/challenges/the-birthday-bar

---
+++ SubArrayDivision.swift
@@ -0,0 +1,59 @@
+import Foundation
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+func birthday(s: [Int], d: Int, m: Int) -> Int {
+    var noOfWays : Int = 0
+    var windowSum : Int = 0
+    for index in 0..<s.count {
+        windowSum = 0
+        for number in index..<index + m {
+            if number < s.count {
+                windowSum += s[number]
+                if number == (index + m) - 1 {
+                    noOfWays = d == windowSum ? noOfWays + 1 : noOfWays
+                }
+            } else {
+                return noOfWays
+            }
+        }
+    }
+    return noOfWays
+}
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let sTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let s: [Int] = sTemp.split(separator: " ").map {
+    if let sItem = Int($0) {
+        return sItem
+    } else { fatalError("Bad input") }
+}
+
+guard s.count == n else { fatalError("Bad input") }
+
+guard let firstMultipleInputTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+let firstMultipleInput = firstMultipleInputTemp.split(separator: " ").map{ String($0) }
+
+guard let d = Int(firstMultipleInput[0])
+else { fatalError("Bad input") }
+
+guard let m = Int(firstMultipleInput[1])
+else { fatalError("Bad input") }
+
+let result = birthday(s: s, d: d, m: m)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
